3cb153b865506377985e40df43bd952d353c61ca
nadaayman20 <76060283+nadaayman20@users.noreply.github.com>
2020-12-15 19:55:38 +0200

    Add files via upload

---
+++ Celebrity jeopardy.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+    char c;
+    while(c=getchar(),c!=EOF){
+        putchar(c);
+    }
+    return 0;
+}
